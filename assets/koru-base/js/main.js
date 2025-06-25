ac96e8ddb136d6e8f5422cce1c83f06ee85e3dfe
eip-pantheon <webmaster+eippantheon@ucar.edu>
2025-06-25 17:51:45 +0000

    Koru-base latest tag update to v2.1.212.

--- assets/koru-base/js/main.js
+++ assets/koru-base/js/main.js
@@ -14,45 +14,18 @@ jQuery(document).ready(function($) {
 
   var $banner_content = $(".banner-content");
   var $dropdown_menu = $("nav.main-nav ul.menu.dropdown");
-  var home_hero_open = false;
 
   $dropdown_menu.on("show.zf.dropdownmenu", function(){
-    if(home_hero_open) {
-      $banner_content.css('visibility', 'hidden');
-    }
-    else {
+    {
       $banner_content.addClass("show-for-sr");
     }
   });
 
   $dropdown_menu.on("hide.zf.dropdownmenu", function(){
-    if(home_hero_open) {
-      $banner_content.css('visibility', 'visible');
-    }
-    else {
+    {
       $banner_content.removeClass("show-for-sr");
     }
   });
-
-  // home_hero_read_more
-  (function() {
-    var $home_hero = $(".home-hero");
-
-    $home_hero.find("a.plus").on("click", function(e) {
-      e.preventDefault();
-      if(home_hero_open === false) {
-        $(this).attr("title", "See Less");
-        $(this).find("i").removeClass("fa-plus").addClass("fa-minus");
-        $home_hero.addClass("open");
-        home_hero_open = true;
-      } else {
-        $(this).attr("title", "See More");
-        $(this).find("i").removeClass("fa-minus").addClass("fa-plus");
-        $home_hero.removeClass("open");
-        home_hero_open = false;
-      }
-    });
-  })();
 
   // make_iframes_responsive
   $("iframe").wrap("<div class='responsive-embed widescreen' />");
